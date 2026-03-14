f28b983f49129209081f89e6191693b577632d6d
Divyam-Mehta <dvmehta1999@gmail.com>
2026-03-14 00:12:39 -0400

    Initial project structure

--- src/sanet_onionsorting/anaconda_env/deactivate_env_vars.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export LD_LIBRARY_PATH=$(echo $LD_LIBRARY_PATH | sed -e "s|${CONDA_PREFIX}/lib:||g" -e "s|:${CONDA_PREFIX}/lib||g" -e "s|${CONDA_PREFIX}/lib||g")
-
--- src/sanet_onionsorting/utils/scripts/delall.sh
+++
@@ -1,3 +0,0 @@
-find . -name "*.npy" -delete
-find . -name "*.jpg" -delete
-find . -name "*images*" -delete
